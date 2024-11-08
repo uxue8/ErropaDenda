9ba2d6c351150b910c0ac4e8bf4970ac66d88a79
uxue8 <uxuitasantos@gmail.com>
2024-11-08 10:19:27 +0100

    anadirBotonyVentanaZerrenda

--- app/src/main/java/com/example/erropadenda/erropagehitu.kt
+++ app/src/main/java/com/example/erropadenda/erropagehitu.kt
@@ -1,6 +1,7 @@
 package com.example.erropadenda
 
 import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -26,6 +27,8 @@ class erropagehitu : AppCompatActivity() {
       lateinit var chBai:CheckBox
       lateinit var chEz: CheckBox
       lateinit var btnGehitu: Button
+        lateinit var btnBuelta: Button
+
       val  ListaMota= arrayOf("Kamiseta","Praka","Jaka","Abrigoa")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +52,7 @@ class erropagehitu : AppCompatActivity() {
         chBai=findViewById(R.id.chBai)
         chEz=findViewById(R.id.chEz)
         btnGehitu=findViewById(R.id.btnErGehi)
+        btnBuelta=findViewById(R.id.btnBuelta)
         val adapter= ArrayAdapter(this,android.R.layout.simple_spinner_item,ListaMota)
         spmota.adapter=adapter
 
@@ -69,6 +73,12 @@ class erropagehitu : AppCompatActivity() {
 
         btnGehitu.setOnClickListener {
             gordeProduktua()
+        }
+
+        btnBuelta.setOnClickListener {
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+
         }
 
 
