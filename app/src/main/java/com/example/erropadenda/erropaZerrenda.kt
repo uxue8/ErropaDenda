e547c3b17cb337b09c96e5b31d9927a71bc5f152
uxue8 <uxuitasantos@gmail.com>
2024-11-08 10:32:28 +0100

    anadirRecycle

--- app/src/main/java/com/example/erropadenda/erropaZerrenda.kt
+++ app/src/main/java/com/example/erropadenda/erropaZerrenda.kt
@@ -9,12 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 class erropaZerrenda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+      //  enableEdgeToEdge()
         setContentView(R.layout.activity_erropa_zerrenda)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+       //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      //      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      //      insets
+      //  }
+
+
+
     }
 }
